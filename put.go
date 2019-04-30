f66212b1fca4ebc6c8054bd1152eae78309dd110
Ben Pate <ben@JARVIS.local>
2019-04-30 11:17:35 -0600

    Bugfix on PUT route
    - now correctly handle putting a "NEW" object into the db

--- put.go
+++ put.go
@@ -18,27 +18,45 @@ func (collection *Collection) Put(roles ...RoleFunc) *Collection {
 
 		// TODO: use SCOPE to load object
 
+		// We'll use this to determine if we're (T) creating a new object, or (F) updating an existing one.
+		isNewObject := false
+
 		// Try to load the record from the database
 		object, err := service.LoadObject(context.Param("id"))
 
 		if err != nil {
-			err = derp.Wrap(err, "presto.Get", "Error loading object", RequestInfo(context)).Report()
-			return context.NoContent(err.Code)
+
+			// If the error is ANYTHING BUT a "Not Found" error, then it's a legitimate error,
+			// and we need to shut this whole thing down right now.
+			if err.NotFound() == false {
+				err = derp.Wrap(err, "presto.Put", "Error loading object", RequestInfo(context)).Report()
+				return context.NoContent(err.Code)
+			}
+
+			// Fall through to here means that this is just a "Not Found", which is
+			// OK at this stage.
+			isNewObject = true
 		}
 
-		// If we are using a cache, then use ETags to verify optimistic locking
-		if cache := collection.getCache(); cache != nil {
-			if etag := context.Request().Header.Get("ETag"); etag != "" {
-				if etag != cache.Get(object.ID()) {
-					return context.NoContent(http.StatusConflict)
+		// Only run these checks if we're updating an existing object:
+		// 1) Make sure that ETags match (for optimistic locking)
+		// 2) Make sure that the user has access to the existing object.
+		if isNewObject == false {
+
+			// If we are using a cache, then use ETags to verify optimistic locking
+			if cache := collection.getCache(); cache != nil {
+				if etag := context.Request().Header.Get("ETag"); etag != "" {
+					if etag != cache.Get(object.ID()) {
+						return context.NoContent(http.StatusConflict)
+					}
 				}
 			}
-		}
 
-		// Check roles (before update) to make sure that we're allowed to touch this object
-		for _, role := range roles {
-			if role(context, object) == false {
-				return context.NoContent(http.StatusUnauthorized)
+			// Check roles (before update) to make sure that we're allowed to touch this object
+			for _, role := range roles {
+				if role(context, object) == false {
+					return context.NoContent(http.StatusUnauthorized)
+				}
 			}
 		}
 
@@ -47,7 +65,7 @@ func (collection *Collection) Put(roles ...RoleFunc) *Collection {
 
 		// Update the object with new information
 		if er := context.Bind(object); er != nil {
-			err := derp.New(derp.CodeBadRequestError, "presto.Put", "Error binding object", er.Error(), object, RequestInfo(context)).Report()
+			err := derp.New(derp.CodeBadRequestError, "presto.Put", "Error binding object", er, object, RequestInfo(context)).Report()
 			return context.NoContent(err.Code)
 		}
 
